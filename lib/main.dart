168e61e35b546b802d00876f105da7bd7c13992b
Samitti <samuelteweldebrhan@gmail.com>
2022-07-15 11:45:28 +0300

    add drawer to shop and orders

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './providers/cart.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
+import './screens/orders_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeNme: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
+          OrdersScreen.routeName: (context) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
 
 import '../screens/cart_screen.dart';
 import '../widgets/products_grid.dart';
@@ -65,6 +66,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: appDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
