1f8210751c5eae313972f3160f0fd84a4fae5817
Samitti <samuelteweldebrhan@gmail.com>
2022-07-28 12:26:28 +0300

    add edit product screen and routing

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
@@ -17,7 +18,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
