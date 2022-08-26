9f143b6ec5222ce4d8c5d23dfffb6ebf3cbd62dd
Samitti <samuelteweldebrhan@gmail.com>
2022-08-26 10:09:27 +0300

    add update data via patch request

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -96,11 +96,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != '') {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
+
       Navigator.of(context).pop();
     } else {
       try {
@@ -122,13 +120,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
+      // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
     // Navigator.of(context).pop();
   }
 
