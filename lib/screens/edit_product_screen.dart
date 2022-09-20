78f216b72ca70a2c1b95668ce96c5ce53f157e9c
Samitti <samuelteweldebrhan@gmail.com>
2022-09-20 14:42:01 +0300

    fixed product update error

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -104,6 +104,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
       try {
         await Provider.of<Products>(context, listen: false)
             .addProduct(_editedProduct);
+        Navigator.of(context).pop();
       } catch (error) {
         await showDialog<Null>(
           context: context,
